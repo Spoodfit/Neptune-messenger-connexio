ff63f7e852f594b8046a3f09c52c61fc814a8fac
Neptune Business <neptunebusinessclub@gmail.com>
2026-07-30 18:24:17 +0200

    feat(storage): expose outbox purge on web previews

--- src/storage/outboxStore.web.ts
+++ src/storage/outboxStore.web.ts
@@ -1,2 +1,5 @@
-export { createMemoryOutboxStore as createOutboxStore } from "./outboxStore.memory";
+export {
+  createMemoryOutboxStore as createOutboxStore,
+  purgeOutboxData
+} from "./outboxStore.memory";
 export type { OutboxStore } from "./outboxStore.types";
