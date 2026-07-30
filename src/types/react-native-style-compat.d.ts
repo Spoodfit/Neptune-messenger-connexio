9eb2411c9b1d700d54442e191500ef47e203cb46
Neptune Business <neptunebusinessclub@gmail.com>
2026-07-31 01:49:55 +0200

    chore: remove obsolete React Native style compatibility

--- src/types/react-native-style-compat.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ViewStyle } from "react-native";
-import "react-native";
-
-declare module "react-native" {
-  interface StyleSheetStatic {
-    readonly absoluteFillObject: ViewStyle;
-  }
-}
