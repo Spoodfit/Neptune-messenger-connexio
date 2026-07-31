22b64b3dc9b3af9c72c4d791db79c35ac4fe255c
Neptune Business <neptunebusinessclub@gmail.com>
2026-08-01 00:57:33 +0200

    feat: exposer la configuration des appels

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ import Constants from "expo-constants";
 interface AppExtra {
   apiBaseUrl?: string;
   realtimeUrl?: string;
+  callBaseUrl?: string;
   mockMode?: boolean;
   eas?: {
     projectId?: string;
@@ -14,6 +15,7 @@ const extra = (Constants.expoConfig?.extra ?? {}) as AppExtra;
 export const env = {
   apiBaseUrl: extra.apiBaseUrl ?? "",
   realtimeUrl: extra.realtimeUrl ?? "",
+  callBaseUrl: extra.callBaseUrl ?? "https://meet.jit.si",
   mockMode: extra.mockMode === true,
   easProjectId: extra.eas?.projectId ?? Constants.easConfig?.projectId ?? ""
 } as const;
