19270114e8846f4f40dcb74432a3665f51c68596
jojoleblond <johanzambelli@gmail.com>
2026-08-14 18:15:15 +0200

    chore: align device contact picker exports

--- src/services/contacts/deviceContactPicker.web.ts
+++ src/services/contacts/deviceContactPicker.web.ts
@@ -1,5 +1,15 @@
 import type { SelectedDeviceContact } from "../../types/deviceContacts";
 
+export class DeviceContactPermissionError extends Error {
+  readonly canAskAgain: boolean;
+
+  constructor(canAskAgain: boolean) {
+    super("CONTACTS_PERMISSION_DENIED");
+    this.name = "DeviceContactPermissionError";
+    this.canAskAgain = canAskAgain;
+  }
+}
+
 export async function pickDeviceContact(): Promise<SelectedDeviceContact | null> {
   return null;
 }
